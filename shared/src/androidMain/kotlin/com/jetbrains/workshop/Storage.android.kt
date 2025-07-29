2e62adaf66b6c650feb0dff66338c893a11073ce
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-07-29 10:28:31 +0200

    Add solutions

--- shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
+++ shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
@@ -4,3 +4,13 @@ import android.app.Application
 import android.content.Context
 
 val context get() = ContextHelper.currentContext!!
+
+actual fun persistString(key: String, value: String) {
+    val file = java.io.File(context.filesDir, key)
+    file.writeText(value)
+}
+
+actual fun restoreString(key: String): String {
+    val file = java.io.File(context.filesDir, key)
+    return if (file.exists()) file.readText() else ""
+}
